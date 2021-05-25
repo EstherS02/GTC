0d7ff48fe2ab9f23a117d1bdf9cd56899344c68c
Sangavi Rajagopal <sangavisanju2101@gmail.com>
2021-05-25 11:17:10 +0530

    Welcome to GTC console added

--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const app = express();
 const request = require("request");
+console.log("Welcome to GTC project")
 
 app.get('/', (req,res)=>{
     request("http://localhost:8080/api/tasks", function (error, response, body) {
         if (error) {
             console.log(error);
         }
-<<<<<<< HEAD
-        console.log("Hello!")
-        console.log("Good morning!")
-=======
         console.log("Hello!");
         console.log("welcome");
->>>>>>> 6364e7399032d0ea90124c3be394b4c42e31baca
         console.log(response);
         console.log(body);
     });
